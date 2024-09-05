ad30f77e320381b10358db6a536394731282597f
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-05 14:12:10 -0400

    Update Transposed_matrix.cpp

--- t1/Transposed_matrix.cpp
+++ t1/Transposed_matrix.cpp
@@ -3,40 +3,95 @@
 using namespace std;
 using namespace std::chrono;
 
-void Transposed_matrix(int A[2][2], int B[2][2], int C[2][2], int D[2][2]){
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
+void Transposed(int n, int** B, int** D){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             D[i][j] = B[j][i];
-        }
-    }
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
-            C[i][j] = 0;
-            for (int k = 0; k < 2; k++){
-                C[i][j] +=  A[i][k] * B[k][j];
-            }
         }
     }
 }
 
-
+void Transposed_matrix(int n, int** A, int** C, int** D){
+     for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++){
+                C[i][j] +=  A[i][k] * D[j][k];
+            }
+        }
+    }   
+}
 int main(){
+
+    int n = 512;
+    int** a = new int*[n];
+    int** b = new int*[n];
+    int** c = new int*[n];
+    int** d = new int*[n];
+
+    for (int i = 0; i < n; i++) {
+        a[i] = new int[n];
+        b[i] = new int[n];
+        c[i] = new int[n];
+        d[i] = new int[n];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = 1+ rand() % 1000;
+            b[i][j] = 1+ rand() % 1000;
+        }
+    }
+    Transposed(n, b, d);
     auto start = high_resolution_clock::now();
-    int a[2][2] = {{1, 2}, {3, 4}};
-    int b[2][2] = {{5, 6}, {7, 8}};
-    int c[2][2];
-    int d[2][2];
-    Transposed_matrix(a, b, c, d);
+    Transposed_matrix(n, a, c, d);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
     cout<<endl;
 
-    for (int i = 0; i < 2; i++){
-        for (int j = 0; j < 2; j++){
+    cout<<"Matriz A: "<<endl;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout<<"Matriz B: "<<endl;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout<<"Matriz B transpuesta: "<<endl;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << d[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout<<"Matriz Resultante: "<<endl;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             cout << c[i][j] << " ";
         }
         cout << endl;
     }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
+
+    for (int i = 0; i < n; i++) {
+        delete[] a[i];
+        delete[] b[i];
+        delete[] c[i];
+    }
+    delete[] a;
+    delete[] b;
+    delete[] c;
+
+
     cout<<"tiempo: "<<duration.count();
     return 0;    
 }

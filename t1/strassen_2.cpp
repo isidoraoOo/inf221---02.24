30c230814c479287f3a44418046e5978f61f65ec
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-05 14:12:43 -0400

    Update strassen_2.cpp

--- t1/strassen_2.cpp
+++ t1/strassen_2.cpp
@@ -1,169 +1,154 @@
-#include <bits/stdc++.h>
-using namespace std::chrono;
+#include <iostream>
+#include <vector>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
-#define ROW_1 4
-#define COL_1 4
+typedef vector<vector<int>> Matrix;
 
-#define ROW_2 4
-#define COL_2 4
+// Función para dividir la matriz en submatrices
+void split(const Matrix& mat, Matrix& a, Matrix& b, Matrix& c, Matrix& d) {
+    int r2 = mat.size() / 2;
+    int c2 = mat[0].size() / 2;
 
-void print(string display, vector<vector<int> > matrix,
-		int start_row, int start_column, int end_row,
-		int end_column)
-{
-	cout << endl << display << " =>" << endl;
-	for (int i = start_row; i <= end_row; i++) {
-		for (int j = start_column; j <= end_column; j++) {
-			cout << setw(10);
-			cout << matrix[i][j];
-		}
-		cout << endl;
-	}
-	cout << endl;
-	return;
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            a[i][j] = mat[i][j];           // top-left
+            b[i][j] = mat[i][j + c2];      // top-right
+            c[i][j] = mat[i + r2][j];      // bottom-left
+            d[i][j] = mat[i + r2][j + c2]; // bottom-right
+        }
+    }
 }
 
-void add_matrix(vector<vector<int> > matrix_A,
-				vector<vector<int> > matrix_B,
-				vector<vector<int> >& matrix_C,
-				int split_index)
-{
-	for (auto i = 0; i < split_index; i++)
-		for (auto j = 0; j < split_index; j++)
-			matrix_C[i][j]
-				= matrix_A[i][j] + matrix_B[i][j];
+// Función para sumar dos matrices
+Matrix add(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix result(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            result[i][j] = A[i][j] + B[i][j];
+    return result;
 }
 
-vector<vector<int> >
-multiply_matrix(vector<vector<int> > matrix_A,
-				vector<vector<int> > matrix_B)
-{
-	int col_1 = matrix_A[0].size();
-	int row_1 = matrix_A.size();
-	int col_2 = matrix_B[0].size();
-	int row_2 = matrix_B.size();
-
-	if (col_1 != row_2) {
-		cout << "\nError: The number of columns in Matrix "
-				"A must be equal to the number of rows in "
-				"Matrix B\n";
-		return {};
-	}
-
-	vector<int> result_matrix_row(col_2, 0);
-	vector<vector<int> > result_matrix(row_1,
-									result_matrix_row);
-
-	if (col_1 == 1)
-		result_matrix[0][0]
-			= matrix_A[0][0] * matrix_B[0][0];
-	else {
-		int split_index = col_1 / 2;
-
-		vector<int> row_vector(split_index, 0);
-		vector<vector<int> > result_matrix_00(split_index,
-											row_vector);
-		vector<vector<int> > result_matrix_01(split_index,
-											row_vector);
-		vector<vector<int> > result_matrix_10(split_index,
-											row_vector);
-		vector<vector<int> > result_matrix_11(split_index,
-											row_vector);
-
-		vector<vector<int> > a00(split_index, row_vector);
-		vector<vector<int> > a01(split_index, row_vector);
-		vector<vector<int> > a10(split_index, row_vector);
-		vector<vector<int> > a11(split_index, row_vector);
-		vector<vector<int> > b00(split_index, row_vector);
-		vector<vector<int> > b01(split_index, row_vector);
-		vector<vector<int> > b10(split_index, row_vector);
-		vector<vector<int> > b11(split_index, row_vector);
-
-		for (auto i = 0; i < split_index; i++)
-			for (auto j = 0; j < split_index; j++) {
-				a00[i][j] = matrix_A[i][j];
-				a01[i][j] = matrix_A[i][j + split_index];
-				a10[i][j] = matrix_A[split_index + i][j];
-				a11[i][j] = matrix_A[i + split_index]
-									[j + split_index];
-				b00[i][j] = matrix_B[i][j];
-				b01[i][j] = matrix_B[i][j + split_index];
-				b10[i][j] = matrix_B[split_index + i][j];
-				b11[i][j] = matrix_B[i + split_index]
-									[j + split_index];
-			}
-
-		add_matrix(multiply_matrix(a00, b00),
-				multiply_matrix(a01, b10),
-				result_matrix_00, split_index);
-		add_matrix(multiply_matrix(a00, b01),
-				multiply_matrix(a01, b11),
-				result_matrix_01, split_index);
-		add_matrix(multiply_matrix(a10, b00),
-				multiply_matrix(a11, b10),
-				result_matrix_10, split_index);
-		add_matrix(multiply_matrix(a10, b01),
-				multiply_matrix(a11, b11),
-				result_matrix_11, split_index);
-
-		for (auto i = 0; i < split_index; i++)
-			for (auto j = 0; j < split_index; j++) {
-				result_matrix[i][j]
-					= result_matrix_00[i][j];
-				result_matrix[i][j + split_index]
-					= result_matrix_01[i][j];
-				result_matrix[split_index + i][j]
-					= result_matrix_10[i][j];
-				result_matrix[i + split_index]
-							[j + split_index]
-					= result_matrix_11[i][j];
-			}
-
-		result_matrix_00.clear();
-		result_matrix_01.clear();
-		result_matrix_10.clear();
-		result_matrix_11.clear();
-		a00.clear();
-		a01.clear();
-		a10.clear();
-		a11.clear();
-		b00.clear();
-		b01.clear();
-		b10.clear();
-		b11.clear();
-	}
-	return result_matrix;
+// Función para restar dos matrices
+Matrix subtract(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix result(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            result[i][j] = A[i][j] - B[i][j];
+    return result;
 }
 
-int main()
-{
+// Algoritmo de Strassen para multiplicar dos matrices
+Matrix strassen(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+
+    // Caso base: si la matriz es 1x1
+    if (n == 1) {
+        Matrix C(1, vector<int>(1));
+        C[0][0] = A[0][0] * B[0][0];
+        return C;
+    }
+
+    // Dividir las matrices en submatrices
+    int newSize = n / 2;
+    Matrix a(newSize, vector<int>(newSize));
+    Matrix b(newSize, vector<int>(newSize));
+    Matrix c(newSize, vector<int>(newSize));
+    Matrix d(newSize, vector<int>(newSize));
+    Matrix e(newSize, vector<int>(newSize));
+    Matrix f(newSize, vector<int>(newSize));
+    Matrix g(newSize, vector<int>(newSize));
+    Matrix h(newSize, vector<int>(newSize));
+
+    split(A, a, b, c, d);
+    split(B, e, f, g, h);
+
+    // Calcular los 7 productos de Strassen
+    Matrix p1 = strassen(a, subtract(f, h));
+    Matrix p2 = strassen(add(a, b), h);
+    Matrix p3 = strassen(add(c, d), e);
+    Matrix p4 = strassen(d, subtract(g, e));
+    Matrix p5 = strassen(add(a, d), add(e, h));
+    Matrix p6 = strassen(subtract(b, d), add(g, h));
+    Matrix p7 = strassen(subtract(a, c), add(e, f));
+
+    // Combinar los resultados en la matriz C
+    Matrix c11 = add(subtract(add(p5, p4), p2), p6);
+    Matrix c12 = add(p1, p2);
+    Matrix c21 = add(p3, p4);
+    Matrix c22 = subtract(subtract(add(p1, p5), p3), p7);
+
+    // Combinar las submatrices en una matriz
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            C[i][j] = c11[i][j];
+            C[i][j + newSize] = c12[i][j];
+            C[i + newSize][j] = c21[i][j];
+            C[i + newSize][j + newSize] = c22[i][j];
+        }
+    }
+
+    return C;
+}
+
+int main() {
+    int n = 512; // Tamaño de la matriz (debe ser una potencia de 2 para el algoritmo de Strassen)
+    
+    // Inicialización de matrices A y B como vectores 2D
+    Matrix A(n, vector<int>(n));
+    Matrix B(n, vector<int>(n));
+
+    // Asignar valores aleatorios a las matrices A y B
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = 1 + rand() % 1000;
+            B[i][j] = 1 + rand() % 1000;
+        }
+    }
+
+    // Iniciar el conteo del tiempo
     auto start = high_resolution_clock::now();
-	vector<vector<int> > matrix_A = { { 1, 1, 1, 1 },
-									{ 2, 2, 2, 2 },
-									{ 3, 3, 3, 3 },
-									{ 2, 2, 2, 2 } };
 
-	print("Array A", matrix_A, 0, 0, ROW_1 - 1, COL_1 - 1);
+    // Multiplicar usando Strassen
+    Matrix C = strassen(A, B);
 
-	vector<vector<int> > matrix_B = { { 1, 1, 1, 1 },
-									{ 2, 2, 2, 2 },
-									{ 3, 3, 3, 3 },
-									{ 2, 2, 2, 2 } };
-
-	print("Array B", matrix_B, 0, 0, ROW_2 - 1, COL_2 - 1);
-
-	vector<vector<int> > result_matrix(
-		multiply_matrix(matrix_A, matrix_B));
-
-	print("Result Array", result_matrix, 0, 0, ROW_1 - 1,
-		COL_2 - 1);
-        
+    // Detener el conteo del tiempo
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
-    cout<<"tiempo: "<<duration.count()<<endl;
-}
 
-// Time Complexity: O(n^3)
-// Code Contributed By: lucasletum
-// Extraido de geeksforgeeks.org
+    // Mostrar las matrices A, B y la matriz resultante C
+    cout << "Matriz A: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Matriz B: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    cout << "Matriz Resultante: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Mostrar el tiempo de ejecución
+    cout<<"tiempo: "<<duration.count();
+
+    return 0;
+}

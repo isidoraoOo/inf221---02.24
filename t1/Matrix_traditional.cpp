6b833a410128dc412a2f1f1a00a1c2646f833789
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-05 22:41:58 -0400

    Update Matrix_traditional.cpp

--- t1/Matrix_traditional.cpp
+++ t1/Matrix_traditional.cpp
@@ -1,71 +1,97 @@
+
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <string>
 #include <cstdlib>
 using namespace std;
 using namespace std::chrono;
 
-void Matrix_multiplication(int n, int** A, int** B, int** C){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+void leerMatriz(ifstream& archivo, int** matriz, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            archivo >> matriz[i][j];
+        }
+    }
+}
+
+void Matrix_multiplication(int n, int** A, int** B, int** C) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             C[i][j] = 0;
-            for (int k = 0; k < n; k++){
+            for (int k = 0; k < n; k++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 }
 
-int main(){
 
-    int n = 512;
-    int** a = new int*[n];
-    int** b = new int*[n];
-    int** c = new int*[n];
+int main() {
+    string nombreArchivo = "matrices.txt"; 
+    ifstream archivo(nombreArchivo);
 
-    for (int i = 0; i < n; i++) {
-        a[i] = new int[n];
-        b[i] = new int[n];
-        c[i] = new int[n];
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo!" << endl;
+        return 1;
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            a[i][j] = 1+ rand() % 1000;
-            b[i][j] = 1+ rand() % 1000;
-        }
+    string nombreMatriz;
+    int filas, columnas;
+    
+    archivo >> nombreMatriz >> filas >> columnas;
+
+    int** a = new int*[filas];
+    for (int i = 0; i < filas; i++) {
+        a[i] = new int[columnas];
     }
+
+    leerMatriz(archivo, a, filas, columnas);
+
+    archivo >> nombreMatriz >> filas >> columnas;
+
+    int** b = new int*[filas];
+    for (int i = 0; i < filas; i++) {
+        b[i] = new int[columnas];
+    }
+
+    leerMatriz(archivo, b, filas, columnas);
+
+    int** c = new int*[filas];
+    for (int i = 0; i < filas; i++) {
+        c[i] = new int[columnas];
+    }
+
+
     auto start = high_resolution_clock::now();
-    Matrix_multiplication(n, a, b, c);
+    Matrix_multiplication(filas, a, b, c);
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
 
-    cout<<"\n"<<endl;
-
-    cout<<"Matriz A: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+    cout << "\nMatriz A:" << endl;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
             cout << a[i][j] << " ";
         }
         cout << endl;
     }
-    cout << endl;
-    cout<<"Matriz B: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+
+    cout << "\nMatriz B:" << endl;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
             cout << b[i][j] << " ";
         }
         cout << endl;
     }
-    cout << endl;
-    cout<<"Matriz Resultante: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+
+    cout << "\nMatriz Resultante (AxB):" << endl;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
             cout << c[i][j] << " ";
         }
         cout << endl;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < filas; i++) {
         delete[] a[i];
         delete[] b[i];
         delete[] c[i];
@@ -74,7 +100,9 @@ int main(){
     delete[] b;
     delete[] c;
 
-    cout<<"tiempo: "<<duration.count();
+    auto duration = duration_cast<nanoseconds>(stop - start);
+    cout << "\nTiempo: " << duration.count() << endl;
 
+    archivo.close();
     return 0;
 }

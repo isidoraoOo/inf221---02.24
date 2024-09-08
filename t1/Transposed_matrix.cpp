4d63f3f2c57b4b5c0cc18026706e5401052191d5
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-07 23:50:52 -0400

    Update Transposed_matrix.cpp
    Redacta los tiempos en un archivo

--- t1/Transposed_matrix.cpp
+++ t1/Transposed_matrix.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
+
+void crear_arch(int n) {
+    ofstream archivo("matrices.txt");
+    archivo << "A " << n << " " << n << "\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int a = rand() % 1000;
+            archivo << a << " ";
+        }
+        archivo << "\n";
+    }
+
+    archivo << "B " << n << " " << n << "\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int b = rand() % 1000;
+            archivo << b << " ";
+        }
+        archivo << "\n";
+    }
+    
+    archivo.close();
+}
+
+void leerMatriz(ifstream& archivo, int** matriz, int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            archivo >> matriz[i][j];
+        }
+    }
+}
 
 void Transposed(int n, int** B, int** D){
     for (int i = 0; i < n; i++){
@@ -21,77 +55,78 @@ void Transposed_matrix(int n, int** A, int** C, int** D){
         }
     }   
 }
-int main(){
 
-    int n = 512;
-    int** a = new int*[n];
-    int** b = new int*[n];
-    int** c = new int*[n];
-    int** d = new int*[n];
+int main() {
+    int iteraciones;
+    cout << "Iteraciones: ";
+    cin >> iteraciones;  
 
-    for (int i = 0; i < n; i++) {
-        a[i] = new int[n];
-        b[i] = new int[n];
-        c[i] = new int[n];
-        d[i] = new int[n];
-    }
+    ofstream file("tiempos mtrx transpuesta.txt");
+    file << "Tiempos de ejecucion\n";
+    file << "Orden\n";
+    
+    for (int j = 1; j <= iteraciones; j++) {
+        file << pow(2, j) << "x" << pow(2, j) << ": ";
+        int n = pow(2, j);
+        crear_arch(n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            a[i][j] = 1+ rand() % 1000;
-            b[i][j] = 1+ rand() % 1000;
+        ifstream archivo("matrices.txt");
+        if (!archivo.is_open()) {
+            cerr << "Error al abrir el archivo!" << endl;
+            return 1;
         }
-    }
-    Transposed(n, b, d);
-    auto start = high_resolution_clock::now();
-    Transposed_matrix(n, a, c, d);
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout<<endl;
 
-    cout<<"Matriz A: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << a[i][j] << " ";
+        string nombreMatriz;
+        int filas, columnas;
+        
+        archivo >> nombreMatriz >> filas >> columnas;
+
+        int** A = new int*[filas];
+        for (int i = 0; i < filas; i++) {
+            A[i] = new int[columnas];
         }
-        cout << endl;
-    }
-    cout << endl;
-    cout<<"Matriz B: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << b[i][j] << " ";
+
+        leerMatriz(archivo, A, filas, columnas);
+
+        archivo >> nombreMatriz >> filas >> columnas;
+
+        int** B = new int*[filas];
+        for (int i = 0; i < filas; i++) {
+            B[i] = new int[columnas];
         }
-        cout << endl;
-    }
-    cout << endl;
 
-    cout<<"Matriz B transpuesta: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << d[i][j] << " ";
+        leerMatriz(archivo, B, filas, columnas);
+
+        int** C = new int*[filas];
+        for (int i = 0; i < filas; i++) {
+            C[i] = new int[columnas];
         }
-        cout << endl;
-    }
-    cout << endl;
-    cout<<"Matriz Resultante: "<<endl;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << c[i][j] << " ";
+        int** D = new int*[filas];
+        for (int i = 0; i < filas; i++) {
+            D[i] = new int[columnas];
         }
-        cout << endl;
+        Transposed(filas, B, D);
+        auto start = high_resolution_clock::now();
+        Transposed_matrix(filas, A, D, C);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<nanoseconds>(stop - start);
+        file << duration.count() << " nanosegundos " << duration.count() / 1000 << " microsegundos " << duration.count() / 1e9 << " segundos" <<"\n";
+
+        for (int i = 0; i < filas; i++) {
+            delete[] A[i];
+            delete[] B[i];
+            delete[] C[i];
+            delete[] D[i];
+        }
+        delete[] A;
+        delete[] B;
+        delete[] C;
+        delete[] D;
+
+        archivo.close();
+        remove("matrices.txt");
     }
 
-    for (int i = 0; i < n; i++) {
-        delete[] a[i];
-        delete[] b[i];
-        delete[] c[i];
-    }
-    delete[] a;
-    delete[] b;
-    delete[] c;
-
-
-    cout<<"tiempo: "<<duration.count();
-    return 0;    
+    file.close();
+    return 0;
 }

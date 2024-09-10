06cd41cc787c48a491936025381e3c1161788423
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-10 20:50:14 -0300

    Update SelectionSort.cpp
    estandar

--- t1/SelectionSort.cpp
+++ t1/SelectionSort.cpp
@@ -1,5 +1,3 @@
-// C++ program for implementation of
-// selection sort
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
@@ -7,7 +5,6 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
-
 
 void read_file(ifstream& file, int arr[], int n){
     for (int i = 0; i < n; i++){
@@ -45,38 +42,35 @@ void printArray(int arr[], int n)
         cout << arr[i] << " ";
     cout << endl;
 }
+int main() {
+    string nombre;
+    int a;
 
-// This is code is contributed by rathbhupendra
+    cout << "Nombre del archivo a ejecutar: " << endl;
+    getline(cin, nombre);
+    string name = nombre + ".txt";
 
-int main(){
-    int iteraciones;
-    cout <<"Iteraciones: ";
-    cin >> iteraciones;
 
-    ofstream file("tiempos SelectionSort ordenados.txt");
-    file << "Tiempos de ejecucion\n";
-    file << "Largo\n";
+    cout << "Largo del vector a ordenar: " << endl;
+    cin >> a;
+    
 
-    for (int j = 1; j <= iteraciones; j++){
-        int a = pow(2, j);
-        string name = "vector largo " + to_string(a) + " ordenado.txt";
-        ifstream archivo(name);
-        if (!archivo.is_open()) {
-            cerr << "Error al abrir el archivo!" << endl;
-            return 1;
-        }
-
-        int* list = new int[a];
-        int largo = sizeof(list) / sizeof(list[0]);
-        read_file(archivo, list, a);
-        archivo.close();
-        auto start = high_resolution_clock::now();
-        selectionSort(list, a);
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<nanoseconds>(stop - start);
-        file << a << ": " << duration.count() << " nanosegundos " << duration.count() / 1000 << " microsegundos " << duration.count() / 1e9 << " segundos" <<"\n";
-        delete[] list;
+    ifstream archive(name);
+    if (!archive.is_open()) {
+        cerr << "Error al abrir el archivo!" << endl;
+        return 1;
     }
-    file.close();
+
+    int* list = new int[a];
+    int largo = sizeof(list) / sizeof(list[0]);
+    read_file(archive, list, a);
+    archive.close();
+    selectionSort(list, a);
+    cout<<"Vector inicial: "<<endl;
+    printArray(list, a);
+    sort(V.begin(), V.end());
+    cout<<"Vector ordenado: "<<endl;
+    printArray(list, a);
+    delete[] list;
     return 0;
 }

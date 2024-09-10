636baec9db8d8bcc419998af98fa7067430d7a3e
Isidora <135176574+isidoraoOo@users.noreply.github.com>
2024-09-10 20:47:11 -0300

    Update Sort.cpp
    estandar

--- t1/Sort.cpp
+++ t1/Sort.cpp
@@ -6,35 +6,46 @@
 using namespace std;
 using namespace std::chrono;
 
-
 void read_file(ifstream& file, vector<int>& V, int n){
     for (int i = 0; i < n; i++){
         file >> V[i];
     }
 }
 
-int main(){
-    int n;
-    cout<<"Iteraciones: ";
-    cin>>n;
+// Function to print a vector
+void printVector(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 
-    ofstream file("tiempos Sort desordenados.txt");
-    file << "Tiempos de ejecucion\n";
-    file << "Largo\n";
+int main() {
+    string nombre;
+    int a;
 
-    for (int it = 1; it <= n; it++){
-        int a = pow(2, it);
-        string name = "vector largo " + to_string(a) + " desordenado.txt";
-        ifstream archive(name);
-        vector<int> V(a);
-        read_file(archive, V, a);
-        archive.close();
-        auto start = high_resolution_clock::now();
-        sort(V.begin(), V.end());
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<nanoseconds>(stop - start);
-        file << a << ": " << duration.count() << " nanosegundos " << duration.count() / 1000 << " microsegundos " << duration.count() / 1e9 << " segundos" <<"\n";
+    cout << "Nombre del archivo a ejecutar: " << endl;
+    getline(cin, nombre);
+    string name = nombre + ".txt";
+
+
+    cout << "Largo del vector a ordenar: " << endl;
+    cin >> a;
+    
+
+    ifstream archive(name);
+    if (!archive.is_open()) {
+        cerr << "Error al abrir el archivo!" << endl;
+        return 1;
     }
-    file.close();
+
+    vector<int> V(a);
+    read_file(archive, V, a);
+    archive.close();
+    cout<<"Vector inicial: "<<endl;
+    printVector(V);
+    sort(V.begin(), V.end());
+    cout<<"Vector ordenado: "<<endl;
+    printVector(V);
     return 0;
 }
